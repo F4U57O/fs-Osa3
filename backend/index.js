6f05dc05e26cb1252ef5611859cee29ffd5feed9
F4U57O <fabio.sciortino@helsinki.fi>
2024-04-04 16:01:32 +0300

    add frontend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
+app.use(express.static('dist'))
 app.use(cors())
 
 morgan.token('request-body', (request, response) => {
