d9951ae52d1a00de92c24bc908482c891f15b27c
F4U57O <fabio.sciortino@helsinki.fi>
2024-04-11 21:24:13 +0300

    part3

--- backend/models/person.js
+++ backend/models/person.js
@@ -9,7 +9,7 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url)
 
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -26,23 +26,23 @@ const personSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate: {
-        validator: function(v) {
-            const parts = v.split('-')
-            if (parts.length !==2) return false
+      validator: function(v) {
+        const parts = v.split('-')
+        if (parts.length !==2) return false
 
-            const firstPart = parts[0]
-            if (firstPart.length !==2 && firstPart.length !== 3) return false
-            if (!/^\d+$/.test(firstPart)) return false
-            
-            const secondPart = parts[1]
-            if (secondPart.length < 5) return false
-            if (!/^\d+$/.test(secondPart)) return false
+        const firstPart = parts[0]
+        if (firstPart.length !==2 && firstPart.length !== 3) return false
+        if (!/^\d+$/.test(firstPart)) return false
 
-            return true
-        },
-        message: props => `${props.value} is not a valid phone number`
+        const secondPart = parts[1]
+        if (secondPart.length < 5) return false
+        if (!/^\d+$/.test(secondPart)) return false
+
+        return true
+      },
+      message: props => `${props.value} is not a valid phone number`
     }
-    }
+  }
 })
 
 personSchema.set('toJSON', {
